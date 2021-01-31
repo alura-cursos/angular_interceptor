9257c0574448bf9013dfd529e615db061c82dd3c
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2021-01-31 14:16:34 -0300

    Ajuste do get

--- gatitobook/src/app/animais/animais.service.ts
+++ gatitobook/src/app/animais/animais.service.ts
@@ -1,7 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { TokenService } from '../autenticacao/token.service';
+import { UsuarioService } from '../autenticacao/usuario/usuario.service';
 import { Animais } from './animais';
 
 const API = environment.apiURL;
@@ -10,9 +12,13 @@ const API = environment.apiURL;
   providedIn: 'root',
 })
 export class AnimaisService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private tokenService: TokenService) {}
 
   listDoUsuario(nomeDoUsuario: string): Observable<Animais> {
-    return this.http.get<Animais>(`${API}/${nomeDoUsuario}/photos`);
+    const token = this.tokenService.retornaToken();
+    const headers = new HttpHeaders().append('x-access-token', token);
+    return this.http.get<Animais>(`${API}/${nomeDoUsuario}/photos`, {
+      headers,
+    });
   }
 }
