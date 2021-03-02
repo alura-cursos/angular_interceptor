7d6b90e75981159bbfa50a176db8d5b315817019
Alvaro <alvaro.camilloneto@gmail.com>
2021-03-02 16:20:12 -0300

    video 2.2

--- gatitobook/src/app/animais/lista-animais/lista-animais.component.ts
+++ gatitobook/src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from 'src/app/autenticacao/usuario/usuario.service';
+import { Animais } from '../animais';
+import { AnimaisService } from '../animais.service';
 
 @Component({
   selector: 'app-lista-animais',
   templateUrl: './lista-animais.component.html',
-  styleUrls: ['./lista-animais.component.css']
+  styleUrls: ['./lista-animais.component.css'],
 })
 export class ListaAnimaisComponent implements OnInit {
+  animais!: Animais;
 
-  constructor() { }
+  constructor(
+    private usuarioService: UsuarioService,
+    private animaisService: AnimaisService
+  ) {}
 
   ngOnInit(): void {
+    this.usuarioService.retornaUsuario().subscribe((usuario) => {
+      const userName = usuario.name ?? '';
+      this.animaisService.listaDoUsuario(userName).subscribe((animais) => {
+        this.animais = animais;
+      });
+    });
   }
-
 }
