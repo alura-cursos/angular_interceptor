5f104f686e96da04ad80fecaf81f2e5c2ce788af
Alvaro <alvaro.camilloneto@gmail.com>
2021-03-02 15:35:57 -0300

    Video 2.1

--- gatitobook/src/app/animais/animais.module.ts
+++ gatitobook/src/app/animais/animais.module.ts
@@ -4,13 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AnimaisRoutingModule } from './animais-routing.module';
 import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
 import { AnimalComponent } from './animal/animal.component';
-
+import { CartaoModule } from '../componentes/cartao/cartao.module';
+import { GradeFotosAnimaisComponent } from './grade-fotos-animais/grade-fotos-animais.component';
 
 @NgModule({
-  declarations: [ListaAnimaisComponent, AnimalComponent],
-  imports: [
-    CommonModule,
-    AnimaisRoutingModule
-  ]
+  declarations: [ListaAnimaisComponent, AnimalComponent, GradeFotosAnimaisComponent],
+  imports: [CommonModule, AnimaisRoutingModule, CartaoModule],
 })
-export class AnimaisModule { }
+export class AnimaisModule {}
