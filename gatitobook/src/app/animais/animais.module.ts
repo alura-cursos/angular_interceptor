28efb628be8b0a8ca09c080cf029260c7995b7ed
Alvaro <alvaro.camilloneto@gmail.com>
2021-03-03 15:05:16 -0300

    Video 3.1

--- gatitobook/src/app/animais/animais.module.ts
+++ gatitobook/src/app/animais/animais.module.ts
@@ -6,9 +6,10 @@ import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
 import { AnimalComponent } from './animal/animal.component';
 import { CartaoModule } from '../componentes/cartao/cartao.module';
 import { GradeFotosAnimaisComponent } from './grade-fotos-animais/grade-fotos-animais.component';
+import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 
 @NgModule({
-  declarations: [ListaAnimaisComponent, AnimalComponent, GradeFotosAnimaisComponent],
+  declarations: [ListaAnimaisComponent, AnimalComponent, GradeFotosAnimaisComponent, DetalheAnimalComponent],
   imports: [CommonModule, AnimaisRoutingModule, CartaoModule],
 })
 export class AnimaisModule {}
---
+++ gatitobook/src/app/animais/detalhe-animal/detalhe-animal.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhe-animal',
+  templateUrl: './detalhe-animal.component.html',
+  styleUrls: ['./detalhe-animal.component.css'],
+})
+export class DetalheAnimalComponent implements OnInit {
+
+
+  constructor(
+  ) {}
+
+  ngOnInit(): void {
+
+  }
+}
