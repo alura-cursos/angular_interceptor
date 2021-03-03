a31c0c4a543d2e261b8075ef7b6cba7e66a40842
Alvaro <alvaro.camilloneto@gmail.com>
2021-03-03 16:51:10 -0300

    Video 3.6

--- gatitobook/src/app/animais/detalhe-animal/detalhe-animal.component.ts
+++ gatitobook/src/app/animais/detalhe-animal/detalhe-animal.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Animal } from '../animais';
 import { AnimaisService } from '../animais.service';
@@ -15,11 +15,29 @@ export class DetalheAnimalComponent implements OnInit {
 
   constructor(
     private animaisService: AnimaisService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
     this.animalId = this.activatedRoute.snapshot.params.animalId;
     this.animal$ = this.animaisService.buscaPorID(this.animalId);
+  }
+
+  curtir() {
+    this.animaisService.curtir(this.animalId).subscribe((curtida) => {
+      if (curtida) {
+        this.animal$ = this.animaisService.buscaPorID(this.animalId);
+      }
+    });
+  }
+
+  excluir() {
+    this.animaisService.excluiAnimal(this.animalId).subscribe(
+      () => {
+        this.router.navigate(['/animais/']);
+      },
+      (error) => console.log(error)
+    );
   }
 }
