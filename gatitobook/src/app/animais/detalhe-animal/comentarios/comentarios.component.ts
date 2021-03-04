8c99366bc41e94d8eb0f75ee4fa2f9806d7c86f2
Alvaro <alvaro.camilloneto@gmail.com>
2021-03-04 15:32:56 -0300

    video 4.3

--- gatitobook/src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
+++ gatitobook/src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
 import { Comentarios } from './comentarios';
+import { ComentariosService } from './comentarios.service';
 
 @Component({
   selector: 'app-comentarios',
@@ -8,9 +11,32 @@ import { Comentarios } from './comentarios';
   styleUrls: ['./comentarios.component.css'],
 })
 export class ComentariosComponent implements OnInit {
+  @Input() id!: number;
   comentarios$!: Observable<Comentarios>;
+  comentarioForm!: FormGroup;
 
-  constructor() {}
+  constructor(
+    private comentariosService: ComentariosService,
+    private formBuilder: FormBuilder
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.comentarios$ = this.comentariosService.buscaComentario(this.id);
+    this.comentarioForm = this.formBuilder.group({
+      comentario: ['', Validators.maxLength(300)],
+    });
+  }
+
+  gravar(): void {
+    const comentario = this.comentarioForm.get('comentario')?.value ?? '';
+    this.comentarios$ = this.comentariosService
+      .incluiComentario(this.id, comentario)
+      .pipe(
+        switchMap(() => this.comentariosService.buscaComentario(this.id)),
+        tap(() => {
+          this.comentarioForm.reset();
+          alert('Salvo Comentário');
+        })
+      );
+  }
 }
