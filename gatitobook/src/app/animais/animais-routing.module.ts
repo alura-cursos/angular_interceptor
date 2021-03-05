929510cd68f846b132347abdabd8a10cedef3870
Alvaro <alvaro.camilloneto@gmail.com>
2021-03-05 15:08:37 -0300

    Video 5.3

--- gatitobook/src/app/animais/animais-routing.module.ts
+++ gatitobook/src/app/animais/animais-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
 import { ListaAnimaisResolver } from './lista-animais/lista-animais.resolver';
+import { NovoAnimalComponent } from './novo-animal/novo-animal.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
     resolve: {
       animais: ListaAnimaisResolver,
     },
+  },
+  {
+    path: 'novo',
+    component: NovoAnimalComponent,
   },
   {
     path: ':animalId',
